68ebf3212cc7dbb9a07a42b885d0ad2d1ce0ea7e
Tjerk Symens <98127222+tjerksymens@users.noreply.github.com>
2023-11-27 19:31:46 +0100

    feat: get messages from the API

--- src/components/Chat/Chat.vue
+++ src/components/Chat/Chat.vue
@@ -7,12 +7,28 @@
         data: ["Heyy", "Hoi", "Hallo"],
     });
 
+    //function to fetch messages from the API
+    const fetchMessages = async () => {
+        try {
+            const response = await fetch("https://message-api-withmongo-lzf4.onrender.com/api/v1/messages/");
+            const data = await response.json();
+            //acces the messages array in the data object and map over it to get the message
+            allMessages.data = data.data[0].messages.map(item => item.message);
+        } catch (error) {
+            console.log(error);
+        }
+        
+    };
+
     // function sendMessage
     const sendMessage = () => {
         allMessages.data.push(message.value);
         message.value = "";
     };
 
+    onMounted(() => {
+        fetchMessages();
+    });
 </script>
 
 <template>
